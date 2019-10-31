86f12d931bd8bbb415e2f522a545a3ab0e769fd3
HavenLau <havenlau2010@hotmail.com>
2019-10-31 23:14:06 +0800

    1

--- Ids4/HavenLau.Ids/Program.cs
+++ Ids4/HavenLau.Ids/Program.cs
@@ -6,13 +6,42 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.AspNetCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 namespace HavenLau.Ids
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Console.Title = "IdentityServerWithEfAndAspNetIdentity";
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"Logs/identityserver4_log.txt")
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                .CreateLogger();
+
+            var seed = args.Contains("/seed");
+            if (seed)
+            {
+                args = args.Except(new[] { "/seed" }).ToArray();
+            }
+
+            var host = CreateHostBuilder(args).Build();
+
+            if (seed)
+            {
+                SeedData.EnsureSeedData(host.Services);
+            }
+
+            host.Run();
             CreateHostBuilder(args).Build().Run();
         }
 
--- Ids4/HavenLau.Ids/Startup.cs
+++ Ids4/HavenLau.Ids/Startup.cs
@@ -7,6 +7,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using IdentityServer4.Quickstart.UI;
+using System.IO;
+using System.Reflection;
+using HavenLau.Ids.Models;
+using HavenLau.Ids.Data;
+
 namespace HavenLau.Ids
 {
     public class Startup
@@ -23,32 +32,53 @@ namespace HavenLau.Ids
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddMvc();
             //services.AddControllersWithViews();
-            var builder = services.AddIdentityServer(options=> {
+            var builder = services.AddIdentityServer(options =>
+            {
                 options.Events.RaiseErrorEvents = true;
                 options.Events.RaiseInformationEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
             })
+            /*
             .AddTestUsers(InMemoryConfig.Users().ToList())
             .AddInMemoryApiResources(InMemoryConfig.GetApiResources().ToList())
-            .AddInMemoryClients(InMemoryConfig.GetClients().ToList());
-
+            .AddInMemoryClients(InMemoryConfig.GetClients().ToList())
+            */
+            .AddAspNetIdentity<ApplicationUser>()
+            .AddConfigurationStore(options=> {
+                options.ConfigureDbContext = b =>
+                b.UseSqlServer(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddOperationalStore(options=> {
+                options.ConfigureDbContext = b =>
+                            b.UseSqlServer(connectionString,
+                                sql => sql.MigrationsAssembly(migrationsAssembly));
+                // 自动清理 token ，可选
+                options.EnableTokenCleanup = true;
+            });
             builder.AddDeveloperSigningCredential();
-
             services.AddAuthentication();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
             app.UseStaticFiles();
-
             app.UseRouting();
-
-            app.UseAuthorization();
+            // app.UseAuthorization();
             app.UseIdentityServer();
             app.UseEndpoints(endpoints =>
             {
@@ -56,6 +86,7 @@ namespace HavenLau.Ids
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            
         }
     }
 }
