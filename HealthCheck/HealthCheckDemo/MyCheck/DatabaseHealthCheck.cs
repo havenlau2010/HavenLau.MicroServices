85e1d6f99e10a344737cb2bbffdada0e6fef62ad
HavenLau <havenlau2010@hotmail.com>
2019-11-04 22:42:53 +0800

    health checks

---
+++ HealthCheck/HealthCheckDemo/MyCheck/DatabaseHealthCheck.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheckDemo.MyCheck
+{
+    public class DatabaseHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            using var connection = new SqlConnection("Data Source=.;Persist Security Info=True;User ID=sa;Password=Lwz3.131492653");
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy());
+            }
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+    }
+}
