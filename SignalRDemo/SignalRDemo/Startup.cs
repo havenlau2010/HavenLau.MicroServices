abb8560f9e89591787f77723c78ee48e512caef6
HavenLau2010 <havenlau2010@hotmail.com>
2019-11-05 17:28:23 +0800

    signalr

--- SignalRDemo/SignalRDemo/Startup.cs
+++ SignalRDemo/SignalRDemo/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SignalRDemo.Hubs;
 
 namespace SignalRDemo
 {
@@ -23,6 +24,7 @@ namespace SignalRDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +49,7 @@ namespace SignalRDemo
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
